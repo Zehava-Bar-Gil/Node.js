4341f8fffb2a9adc573a1721b8a2e0d91a5ae291
Zehava-Bar <zehava.bargil@gmail.com>
2021-04-12 01:11:51 +0300

    .

---
+++ Exercises/1.1 node vs js/index.js
@@ -0,0 +1,17 @@
+/*----------------------------- 1 -----------------------------------------
+What are 3 things node.js has that the client side javascript doesn’t have:
+1. Nodejs does not have capability to add HTML tags 
+   and JS is capable enough to add HTML and play with the DOM.
+2. NodeJS code can be run outside the browser and JS can only be run in the browsers.
+3. Some of the Nodejs modules are Lodash, express etc. These modules are to be imported from npm
+   and Some of the JS frameworks are RamdaJS, TypedJS, etc.
+------------------------------------------------------------------------------------------------*/
+
+/*----------------------------- 2 -----------------------------------------
+What are 3 things the client side javascript has that node.js doesn’t have:
+1. Javascript is used in frontend development and Nodejs is used in server-side 
+   development.
+2. 	Javascript can run in any browser engine as like JS core in safari and Spidermonkey 
+    in Firefox and Nodejs can only run in V8 engine of google chrome. 
+3. JS is basically used on the client-side and node.js is mostly used on the server-side.  
+------------------------------------------------------------------------------------------*/
