f55c13d92f3d6af1d591e9a69eb7195628b5c17f
Zehava-Bar <zehava.bargil@gmail.com>
2021-04-12 01:10:40 +0300

    .

---
+++ Exercises/3.1 Import_Export/app.js
@@ -0,0 +1,3 @@
+const { add, subtract, multy } = require('./utils');
+
+console.log(add(3,7), subtract(2,4), multy(1,1));
---
+++ Exercises/3.1 Import_Export/utils.js
@@ -0,0 +1,13 @@
+function add(x, y) {
+    return x + y;
+ }
+   
+ function subtract(x, y) {
+    return x - y;
+ }
+ 
+ function multy(x, y) {
+    return x * y;
+ }
+ 
+ module.exports = { add, subtract, multy }
